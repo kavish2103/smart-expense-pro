5689b90a8b251efcadbfb010e5b69418baf031ed
Kavish Singhal <kavishsinghal05@gmail.com>
2025-12-16 20:15:54 +0530

    feat: add request logging and global error handling middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import healthRoutes from "./routes/health.route";
+import { logger } from "./middlewares/logger.middleware";
+import { errorHandler } from "./middlewares/error.middleware";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(logger); 
 app.use(healthRoutes);
+app.use(errorHandler);
 
 export default app;
